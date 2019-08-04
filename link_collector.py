fa69b9810a37e358ee36d16f52459e692293bf09
iliskhan_gudaev <iliskhan234@gmail.com>
2019-08-04 22:05:35 +0300

    initial commit

---
+++ link_collector.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+
+import time
+import csv
+
+def csv_writer(data, path):
+
+	with open(path, "w", newline='') as csv_file:
+		writer = csv.writer(csv_file, delimiter='\n')
+		writer.writerow(data)
+
+def get_links(driver):
+	links = []
+	pathes = driver.find_elements_by_class_name('v1Nh3')
+	
+	for p in pathes:
+	
+		link = p.find_element_by_tag_name('a')
+		links.append(link.get_attribute('href'))
+
+	return links
+
+def main():
+
+	url = 'https://www.instagram.com/groznytv/'
+
+	driver = webdriver.Chrome()
+
+	driver.get(url)
+
+	SCROLL_PAUSE_TIME = 6
+
+	last_height = driver.execute_script("return document.body.scrollHeight")
+	
+	links = []
+	links.extend(get_links(driver))
+
+	while True:
+
+		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+		time.sleep(SCROLL_PAUSE_TIME)
+
+		new_height = driver.execute_script("return document.body.scrollHeight")
+
+		if new_height == last_height:
+			break
+		
+		links.extend(get_links(driver))
+
+		last_height = new_height
+
+	links = set(links)
+	print(len(links))
+	csv_writer(links, 'links.csv')
+
+	driver.close()
+
+if __name__ == '__main__':
+	main()
---
+++ scraper.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+
+import pandas as pd 
+
+import excel_exporter
+import time
+import sys
+
+def get_comment(driver, path_to_post):
+    user_names = []
+    user_comments = []
+
+    driver.get(path_to_post)
+    try:
+        close_button = driver.find_element_by_class_name('xqRnw')
+        close_button.click()
+    except:
+        pass
+
+
+    try:
+        load_more_comment = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/article/div[2]/div[1]/ul/li[2]/button')
+        i = 0
+        while load_more_comment.is_displayed() and i < sys.argv[2]:
+            load_more_comment.click()
+            i += 1
+            time.sleep(3)
+    except:
+        pass
+
+
+    comment = driver.find_elements_by_class_name('gElp9 ')
+    for c in comment:
+        container = c.find_element_by_class_name('C4VMK')
+        name = container.find_element_by_class_name('_6lAjh').text
+        content = container.find_element_by_tag_name('span').text
+        content = content.replace('\n', ' ').strip().rstrip()
+        user_names.append(name)
+        user_comments.append(content)
+
+    user_names.pop(0)
+    user_comments.pop(0) 
+    return user_names, user_comments
+
+
+def main():
+
+    driver = webdriver.Chrome()
+
+    user_names = []
+    user_comments = []
+
+    links = pd.read_csv('links.csv',header=None)
+    
+    for idx, link in links.iterrows():
+        names, comments = get_comment(driver, link[0])
+        user_names.extend(names)
+        user_comments.extend(comments)
+
+    print(len(user_names), len(user_comments))
+    excel_exporter.export(user_names, user_comments)
+
+
+    driver.close()
+
+if __name__ == '__main__':
+    main()
