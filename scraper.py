603617e7f56c476eb97890d70d7be2825d206769
iliskhan <iliskhan234@gmail.com>
2019-08-13 05:08:17 +0300

    correct comment collecting

--- scraper.py
+++ scraper.py
@@ -9,7 +9,6 @@ import sys
 import os
 
 def get_comment(driver, path_to_post):
-    user_names = []
     user_comments = []
 
     driver.get(path_to_post)
@@ -23,7 +22,7 @@ def get_comment(driver, path_to_post):
     try:
         load_more_comment = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/article/div[2]/div[1]/ul/li[2]/button')
         i = 0
-        while load_more_comment.is_displayed() and i < sys.argv[2]:
+        while load_more_comment.is_displayed():
             load_more_comment.click()
             i += 1
             time.sleep(3)
@@ -37,16 +36,13 @@ def get_comment(driver, path_to_post):
     comment = driver.find_elements_by_class_name('gElp9 ')
     for c in comment:
         container = c.find_element_by_class_name('C4VMK')
-        name = container.find_element_by_class_name('_6lAjh').text
         content = container.find_element_by_tag_name('span').text
         content = content.replace('\n', ' ').strip().rstrip()
-        user_names.append(name)
         user_comments.append(content)
 
-    user_names.pop(0)
     user_comments.pop(0)
 
-    return description, user_names, user_comments
+    return description, user_comments
 
 
 def main():
@@ -59,11 +55,14 @@ def main():
     with open('comments.json','w', encoding='utf-8') as f_json:
         f_json.write('[\n')
         for idx, link in tqdm(links.iterrows()):
-            description, names, comments = get_comment(driver, link[0])
-            data = {'описание': description, 'комменты': comments}
-            out = json.dumps(data, ensure_ascii=False)
-            print(f'\t{out},', file=f_json)
-
+            try:
+                description, comments = get_comment(driver, link[0])
+                data = {'описание': description, 'комменты': comments}
+                out = json.dumps(data, ensure_ascii=False)
+                print(f'\t{out},', file=f_json)
+            except:
+                pass
+                
         f_json.seek(0, os.SEEK_END)
         f_json.seek(f_json.tell() - 3, os.SEEK_SET)
         f_json.write('\n]')
